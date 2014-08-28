53183fd38a0ea4a0d11780b0d14fb2634f6b79a2
darryl west <darryl.west@RainCitySoftware.com>
2014-08-28 09:24:46 -0700

    implement browser access client

--- browser/AccessClient.js
+++ browser/AccessClient.js
@@ -11,29 +11,48 @@ var AccessClient = function(options) {
     var client = this,
         log = options.log,
         createDigest = options.createDigest,
-        user;
+        user = options.user,
+        socketHost = options.socketHost,
+        hub,
+        accessChannel,
+        privateChannel;
 
-    this.createPrivateChannel = function() {
+    this.createHub = function() {
+        if (!hub) {
+            hub = new Faye.Client( socketHost, { timeout:10 });
+        }
+
+        return hub;
+    };
+
+    this.openAccessChannel = function() {
+        log.info('open the access channel');
+
+        accessChannel = client.createHub().subscribe( '/access', client.accessMessageHandler );
+        accessChannel.then(function() {
+            accessChannel.ready = true;
+        });
+
+        return accessChannel;
+    };
+
+    this.accessMessageHandler = function(msg) {
+        log.debug( JSON.stringify( msg ));
+
+        
+    };
+
+    this.openPrivateChannel = function() {
         log.info('create the private channel');
     };
 
     this.createAuthMessage = function() {
-    };
-
-
-    this.fetchUser = function(id, callback) {
-        // simulate an ajax fetch...
-        var accessUser = {
-                id:'33e2c605f0334ce7a455ec8f6a60e063',
-                session:'2a7f543a-5a4e-44db-a150-b653852ed0c0',
-                privateChannel:'/44C4UUX'
-            };
-
-        callback( null, accessUser );
+        log.info('create the auth request message');
     };
 
     // constructor validations
-    if (!log) throw new Error('server must be constructed with a log');
+    if (!log) throw new Error('access client must be constructed with a log');
+    if (!user) throw new Error('access client must be constructed with a user');
 };
 
 AccessClient.createInstance = function() {
@@ -41,12 +60,25 @@ AccessClient.createInstance = function() {
 
     var opts = {};
 
+    opts.version = '2014.08.28';
+
     opts.log = RemoteLogger.createLogger('AccessClient');
     opts.createDigest = function(key) {
         return CryptoJS.algo.HMAC.create( CryptoJS.algo.SHA256, key );
     };
 
+    // simulate an ajax fetch...
+    opts.user  = {
+        id:'33e2c605f0334ce7a455ec8f6a60e063',
+        session:'2a7f543a-5a4e-44db-a150-b653852ed0c0',
+        privateChannel:'/44C4UUX'
+    };
 
+    opts.host = 'http://localhost:29169';
+    opts.hubName = '/MessageHub';
+    opts.appkey = 'b55d91a2-a68f-48a1-8f4b-c4dfc65d60bb';
+
+    opts.socketHost = opts.host + opts.hubName;
 
     return new AccessClient( opts );
 };
